83270a6b4a73205fb60c4e1d9bcdacb4f32d0b95
Teodor Dimitrov <teodor@kipo.bg>
2021-01-12 13:35:37 +0200

    refactoring reducers with utility functions

--- burger-builder/src/store/reducers/order.js
+++ burger-builder/src/store/reducers/order.js
@@ -1,4 +1,7 @@
 import * as actionTypes from '../actions/actionTypes';
+import {updateObject} from '../utility';
+
+
 
 const initialState = {
   orders: [],
@@ -6,73 +9,64 @@ const initialState = {
   purchased: false
 };
 
+const purchaseInit = (state) => {
+  return updateObject(state, {purchased: false});
+};
+
+const purchaseBurgerStart = (state) => {
+  return updateObject(state, {loading: true});
+};
+const purchaseBurgerSuccess = (state, action) => {
+  const newOrder = updateObject(action.orderData, {
+    id: action.orderId
+  })
+  return updateObject(state, {
+    loading: false,
+    orders: state.orders.concat(newOrder),
+    purchased: true
+  });
+};
+const purchaseBurgerFail = (state) => {
+  return updateObject(state, {loading: false});
+};
+const fetchOrdersStart = (state) => {
+  return updateObject(state, {loading: true});
+};
+const fetchOrdersSuccess = (state, action) => {
+  return updateObject(state, {
+    orders: action.orders,
+    loading: false
+  });
+};
+const fetchOrdersFail = (state) => {
+  return updateObject(state, {loading: false});
+};
+const deleteOrdersStart = (state) => {
+  return updateObject(state, {loading: true});
+};
+const deleteOrdersSuccess = (state, action) => {
+  return updateObject(state, {
+    orders: state.orders.filter(order => order.id !== action.orderId),
+    loading: false
+  });
+};
+const deleteOrdersFail = (state) => {
+  return updateObject(state, {loading: false});
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.PURCHASE_INIT: 
-      return {
-          ...state,
-          purchased: false
-      }
-
-    case actionTypes.PURCHASE_BUGRER_START: 
-      return {
-        ...state,
-        loading: true
-      }
-    case actionTypes.PURCHASE_BUGRER_SUCCESS:
-      const newOrder = {
-        ...action.orderData,
-        id: action.orderId,
-      };
-      return {
-          ...state,
-          loading: false,
-          orders: state.orders.concat(newOrder),
-          purchased: true
-      };
-      
-      case actionTypes.PURCHASE_BUGRER_FAIL:
-        return {
-            ...state,
-            loading: false
-          }
-
-      case actionTypes.FETCH_ORDERS_START: 
-        return {
-          ...state,
-          loading: true
-        }
-      case actionTypes.FETCH_ORDERS_SUCCESS: 
-        return {
-          ...state,
-          orders: action.orders,
-          loading: false
-        }
-      case actionTypes.FETCH_ORDERS_FAIL: 
-        return {
-          ...state,
-          loading: false
-        }
-      case actionTypes.DELETE_ORDER_START: 
-        return {
-          ...state,
-          loading: true
-        }
-
-        case actionTypes.DELETE_ORDER_SUCCESS: 
-          return {
-            ...state,
-            orders: state.orders.filter(order => order.id !== action.orderId),
-            loading: false
-          }
-        case actionTypes.DELETE_ORDER_SUCCESS: 
-          return {
-            ...state,
-            loading: false
-          }
-
-    default:
-      return state;
+    case actionTypes.PURCHASE_INIT: return purchaseInit(state);
+    case actionTypes.PURCHASE_BUGRER_START: return purchaseBurgerStart(state);
+    case actionTypes.PURCHASE_BUGRER_SUCCESS: return purchaseBurgerSuccess(state, action);
+    case actionTypes.PURCHASE_BUGRER_FAIL: return purchaseBurgerFail(state);
+    case actionTypes.FETCH_ORDERS_START: return fetchOrdersStart(state);
+    case actionTypes.FETCH_ORDERS_SUCCESS: return fetchOrdersSuccess(state, action);
+    case actionTypes.FETCH_ORDERS_FAIL: return fetchOrdersFail(state);
+    case actionTypes.DELETE_ORDER_START: return deleteOrdersStart(state);
+    case actionTypes.DELETE_ORDER_SUCCESS: return deleteOrdersSuccess(state, action);
+    case actionTypes.DELETE_ORDER_FAIL: return deleteOrdersFail(state);
+    default: return state;
   }
 };
 
